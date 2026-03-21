17be13e3c1e852e5f04cd84839c9c92da347f4f8
Vipershroom <undertalefan812@gmail.com>
2026-03-21 15:19:07 -0400

    part2 day3 complete

--- python/2022/day3.py
+++ python/2022/day3.py
@@ -21,9 +21,25 @@ def get_priority(puzzle_inp: list[str]) -> int:
         prio += sum(list(map(lambda x: alphabet[x],inter_chars)))
     return prio
 
+def get_priority2(puzzle_inp: list[str]) -> int: 
+    alphabet = {string.ascii_letters[priority - 1]: priority for priority in range(1, len(string.ascii_letters)+1)}
+    prio = 0
+    for rucksack in range(0, len(puzzle_inp), 3):
+        if rucksack + 2 >= len(puzzle_inp):
+            break
+        set1 = set(puzzle_inp[rucksack])
+        set2 = set(puzzle_inp[rucksack + 1])
+        set3 = set(puzzle_inp[rucksack + 2])
+        inter_chars = set1.intersection(set2).intersection(set3) 
+        print(inter_chars)
+        prio += sum(list(map(lambda x: alphabet[x],inter_chars)))
+        
+    return prio
 
 puzzle_inp = list(map(lambda x: x.strip(),get_input('puzzle_inputs/day3.txt')))
 # print(puzzle_inp)
-print(get_priority(puzzle_inp))
+# print(get_priority(puzzle_inp))
 test = ["vJrwpWtwJgWrhcsFMMfFFhFp" ,"jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL", "PmmdzqPrVvPwwTWBwg", "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn", "ttgJtRGJQctTZtZT" ,"CrZsJsPPZsGzwwsLwLmpwMDw"]
-print(get_priority(test))
+# print(get_priority(test))
+print(get_priority2(puzzle_inp))
+print(get_priority2(test))
