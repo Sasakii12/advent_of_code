384e8e500e310d8f7eaf4123938c03616829d5e1
lucascerqueirabank@gmail.com <undertalefan812@gmail.com>
2026-03-31 14:02:05 -0400

    rewrote day1 part 1 code

--- c/day1.c
+++ c/day1.c
@@ -2,68 +2,78 @@
 #include <stdlib.h>
 #include <string.h>
 
-char** read_file(int* count) {
+char** read_input(int *count) {
 	FILE *file;
-
 	file = fopen("puzzle_input/day1.txt", "rb");
-	fseek(file, 0l, SEEK_END);
-	size_t file_size = ftell(file);
-	printf("size: %zu\n", file_size);
+	fseek(file, 0, SEEK_END);
+	size_t size = ftell(file);
 	rewind(file);
-	file = fopen("puzzle_input/day1.txt", "r");
 
-	char** str = (char**) malloc(file_size * sizeof(char*));
+	char** str = (char**)malloc(size * sizeof(char*));
 	char buf[256];
-
 	int i = 0;
-	while (fgets(buf, sizeof(buf), file) != NULL) {
+	
+	while (fgets(buf, 256, file) != NULL) {
 		str[i] = malloc(strlen(buf) + 1);
 		strcpy(str[i], buf);
 		i++;
 	}
-	fclose(file);
 	*count = i;
 	return str;
 }
 
-long int sum(long int *arr, int size) {
-	long int s = 0;
-	for (int i = 0; i < size; i++) {
+long int sum(long int *arr, int len) {
+	int s = 0;
+	for (int i = 0; i < len; i++) {
 		s += arr[i];
 	}
 	return s;
-} 
+}
 
-long int max_num(long int *arr, int count) {
-	long int max_val = arr[0];
-	for (int i = 0; i < count; i++) {
-		if (max_val < arr[i]) {
-			max_val = arr[i];
+long int max_of_arr(long int *arr, int len) {
+	long int max_of_arr = arr[0];
+	for (int i = 1; i < len; i++) {
+		if (max_of_arr < arr[i]) {
+			max_of_arr = arr[i];
 		}
 	}
-	return max_val;
+	return max_of_arr;
+}
+
+long int* arr_of_max(char** str, int len, int* max_len) {
+	char *endptr; 
+	long int *arr_buf = malloc(10 * sizeof(long int));
+	int sum_counter = 0;
+	int counter = 0;
+	long int *arr_of_max = malloc(len * sizeof(long int));
+	for (int i = 0; i < len; i++) {		
+		if (str[i][0] == '\n') {
+			arr_of_max[sum_counter] = sum(arr_buf, counter);
+			counter = 0;
+			sum_counter++;
+			continue;
+		}
+		arr_buf[counter] = strtol(str[i], &endptr, 10);
+		if (endptr == str[i]) {
+			printf("No diigits found\n");
+			return NULL;
+		} else if (*endptr == '\0') {
+			printf("Invalid character %c\n", *endptr);
+			return NULL;
+		}
+		counter++; }
+	*max_len = sum_counter;
+	return arr_buf;
 }
 
 int main(void) {
-	printf("Hello world!\n");
 	int len;
-	char **str = read_file(&len);
-	long int *arr = malloc(len * sizeof(long int));
-	int counter = 0;
-	int sum_count = 0;
-	long int *s = malloc(len * sizeof(long int));
-	char *endptr;
-	for (int i = 0; i < len; i++) {
-		printf("count: %d, string:%s\n", counter, str[i]);
- 		if (str[i][0] == '\n') {
-			s[sum_count] = sum(arr, counter);
-			counter = 0;
-			sum_count++;
-			continue;
-		}
-		arr[counter] = strtol(str[i], &endptr, 10);
-		counter++;
-	}
-	printf("%ld\n", s[1]);
-	printf("%ld\n", max_num(s, sum_count));
- }
+	int max_len;
+	char **str = read_input(&len);
+	long int *arr_max = arr_of_max(str, len,&max_len);
+	long int max = max_of_arr(arr_max, max_len);
+	printf("%ld\n", max);
+
+	free(str);
+	free(arr_max);
+}
