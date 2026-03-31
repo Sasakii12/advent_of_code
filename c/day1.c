70f6f8a85fd6eb96952e72dd3992c68b5b7ebfbe
lucascerqueirabank@gmail.com <undertalefan812@gmail.com>
2026-03-31 13:12:33 -0400

    C day1 complete

--- c/day1.c
+++ c/day1.c
@@ -26,7 +26,7 @@ char** read_file(int* count) {
 	return str;
 }
 
-long int sum(long int *arr, size_t size) {
+long int sum(long int *arr, int size) {
 	long int s = 0;
 	for (int i = 0; i < size; i++) {
 		s += arr[i];
@@ -34,20 +34,36 @@ long int sum(long int *arr, size_t size) {
 	return s;
 } 
 
+long int max_num(long int *arr, int count) {
+	long int max_val = arr[0];
+	for (int i = 0; i < count; i++) {
+		if (max_val < arr[i]) {
+			max_val = arr[i];
+		}
+	}
+	return max_val;
+}
+
 int main(void) {
 	printf("Hello world!\n");
 	int len;
 	char **str = read_file(&len);
 	long int *arr = malloc(len * sizeof(long int));
 	int counter = 0;
+	int sum_count = 0;
+	long int *s = malloc(len * sizeof(long int));
 	char *endptr;
 	for (int i = 0; i < len; i++) {
 		printf("count: %d, string:%s\n", counter, str[i]);
-		if (str[i][0] == '\n') {
-			break;
+ 		if (str[i][0] == '\n') {
+			s[sum_count] = sum(arr, counter);
+			counter = 0;
+			sum_count++;
+			continue;
 		}
-		arr[i] = strtol(str[i], &endptr, 10);
+		arr[counter] = strtol(str[i], &endptr, 10);
 		counter++;
 	}
-	printf("%ld\n", sum(arr, counter));
+	printf("%ld\n", s[1]);
+	printf("%ld\n", max_num(s, sum_count));
  }
